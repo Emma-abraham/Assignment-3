ddf7f53a46a5af9c485bafcb26fe2355428bc258
wagbramu <wagbramu@uwaterloo.ca>
2019-11-05 16:26:49 -0500

    Sir Fresh

--- ece650-a1.py
+++ ece650-a1.py
@@ -5,14 +5,14 @@ import sys
 import operator
 import time
 import os
-    
+
 
 class Assignment_1:
-    
 
-        def __init__(st):   # defining and initializing the st and self objects 
-    
-            
+
+        def __init__(st):   # defining and initializing the st and self objects
+
+
             st.street_nodes = {}                # key:value street_name:[street_nodes]
             st.V = {}                           # key:value assigned vertex_id:{vertex coordinates}
             st.E = []                           # elements: connects two vertex ids from V
@@ -23,26 +23,26 @@ class Assignment_1:
             st.maxid = 0                        # the max id for the current vertex
             st.end_re = re.compile("^exit$")    # the regular expression for the pattern of closing the program
             st.error_messages = {               # list of possible errors key:value
-                "wrong_input_format"    : "Error: input does not match the required format.\n" , 
+                "wrong_input_format"    : "Error: input does not match the required format.\n" ,
                 "street_does_not_exist" : "Error: 'c' or 'r' specified for a street that does not exist.\n",
                 "insufficient_nodes"    : "Error: 'a' or 'c' should specify at least two nodes.\n",
                 "illegal_node_value"    : "Error: the values in coordinates should be only integer or float numbers.\n" ,
                 "add_existing_street"   : "Error: 'a' specified an existing street, use 'c' instead.\n",
                 "wrong_coordinates"     : "Error: Coordinates specified without right format, there should be no spaces between.\n",
                 "system_error"          : "Error: I'm sorry this is an unknown system_error. Restart your program. \n"
-            }   
+            }
 
         def get_command(st, command):   # Defining all commands from the standard input
 
             while(not re.compile('^g$').search(command) and not st.end_re.search(command)):
-                if command != "+" and st.command_handler(command):  
+                if command != "+" and st.command_handler(command):
 
                     st.V.clear()
                     del st.E[0:]
                     st.intersections.clear()
                     st.temp_list.clear()
                     st.populate_tree()
-    
+
 
                 try:
                     command = raw_input()
@@ -50,12 +50,12 @@ class Assignment_1:
                     command = "exit"
 
             return command
-    
+
 
         def run_sim(st):
 
             while(1):
-            
+
                 command = '+'
                 try:
                     command = st.get_command(command)
@@ -67,23 +67,23 @@ class Assignment_1:
                     break
                 else:
                     sys.stderr.write(st.error_messages["system_error"])
-    
-        def does_street_exist(st, street_name): # Check for existence of the street 
+
+        def does_street_exist(st, street_name): # Check for existence of the street
 
             if street_name in st.street_nodes.keys():
                 return True
             else:
                 return False
-    
+
 
         def command_handler(st, command):# Handles all input commands, the exceptions & errors and stores streets as its nodes
-            
+
             ac_command_re = re.compile("^(a|c)\s+(\"\s*[A-Za-z]+\s*[A-Za-z]+\s*[A-Za-z]+\s*[A-Za-z]+\s*\")\s+((\(\s*[-]?[0-9]{1,3}?\s*\,\s*[-]?[0-9]{1,3}?\s*\)+\s*)+)$")
             remv_command_re = re.compile("^r\s(\"(.*)\")$")
             graph_command_re = re.compile('^g$')
             node_re = re.compile("(.*),(.*)\)")
-    
-            
+
+
             if ac_command_re.search(command):
                 data = ac_command_re.findall(command)[0]
 
@@ -91,15 +91,15 @@ class Assignment_1:
                 street_name = data[1].lower()
                 nodes = data[2].strip(" ").split("(")
                 del nodes[0]
-    
+
 
                 if len(nodes) < 2:
                     #sys.stderr.write(st.error_messages["insufficient_nodes"])
                     return False
-    
+
 
                 nodes_tuple = []
-    
+
 
                 if st.does_street_exist(street_name):
                     if action == 'a':
@@ -109,13 +109,13 @@ class Assignment_1:
                     if action == 'c':
                         sys.stderr.write(st.error_messages["street_does_not_exist"])
                         return False
-    
+
 
                 for node in nodes:
                     if not node_re.search(node):
                         sys.stderr.write(st.error_messages["wrong_coordinates"])
                         return False
-    
+
 
                     node_data = node_re.findall(node)[0]
                     try:
@@ -124,11 +124,11 @@ class Assignment_1:
                     except ValueError:
                         sys.stderr.write(st.error_messages["illegal_node_value"])
                         return False
-    
+
 
                 st.street_nodes[street_name] = nodes_tuple
                 return True
-    
+
 
             elif remv_command_re.search(command):
 
@@ -149,52 +149,52 @@ class Assignment_1:
         def populate_tree(st): # getting the nodes and the edges
 
             street_names = st.street_nodes.keys()
-    
+
             # getting the intersections
             for street_i in range(0, len(street_names) - 1):
 
                 for street_j in range (street_i + 1, len(street_names)):
-    
+
 
                     street_i_name = street_names[street_i]
                     street_j_name = street_names[street_j]
-    
+
 
                     st.street_lines[street_i_name] = set()
                     st.street_lines[street_j_name] = set()
-    
+
 
                     street_i_nodes = st.street_nodes[street_i_name]
                     street_j_nodes = st.street_nodes[street_j_name]
-    
+
 
                     for node_i in range(0, len(street_i_nodes) - 1):
-    
+
 
                         for node_j in range(0, len(street_j_nodes) - 1):
-    
+
 
                             line_i = (street_i_nodes[node_i], street_i_nodes[node_i + 1])
                             line_j = (street_j_nodes[node_j], street_j_nodes[node_j + 1])
-    
+
 
                             st.street_lines[street_i_name].add(line_i)
                             st.street_lines[street_j_name].add(line_j)
-    
+
 
                             inter_point = st.calculate(line_i, line_j)
                             if inter_point != False:
                                 # print point
                                 st.intersections.add(inter_point)
-    
+
 
             # for every line with intersection, separate and store into a temporary list
             if len(st.intersections) > 0:
                 st.is_intersected()
-    
+
 
         def is_intersected(st): # getting E and V
-    
+
 
             for intersection in st.intersections:
                 for street_name in st.street_lines.keys():
@@ -205,22 +205,22 @@ class Assignment_1:
                             (line_0, line_1) = line
                             add_line_0 = (intersection, line_0)
                             add_line_1 = (intersection, line_1)
-    
+
 
                             temp_remove_list.append(line)
                             temp_add_list.append(add_line_0)
                             temp_add_list.append(add_line_1)
-    
+
 
                     for remove_line in temp_remove_list:
                         st.street_lines[street_name].remove(remove_line)
                         if remove_line in st.temp_list:
                             st.temp_list.remove(remove_line)
-                    
+
                     for add_line in temp_add_list:
                         st.street_lines[street_name].add(add_line)
                         st.temp_list.add(add_line)
-    
+
 
             # separating V
             nodes = set()
@@ -231,7 +231,7 @@ class Assignment_1:
                     if node not in st.V_ids.values():
                         st.maxid += 1
                         st.V_ids[st.maxid] = node
-    
+
 
             for edge in edges:
                 (node_0, node_1) = edge
@@ -247,84 +247,84 @@ class Assignment_1:
                     if node_id_0 and node_id_1:
                         break
                 st.E.append((node_id_0, node_id_1))
-    
 
-        def is_point_on_line(st, intersection, line): # Checking whether a point is on the line   
+
+        def is_point_on_line(st, intersection, line): # Checking whether a point is on the line
 
             (inter_x, inter_y) = intersection
             (start_x, start_y) = line[0]
             (end_x, end_y) = line[1]
-    
+
             diffrnce_x_inter = inter_x - start_x
             diffrnce_y_inter = inter_y - start_y
 
             diffrnce_x_line = end_x - start_x;
             diffrnce_y_line = end_y - start_y;
-    
+
             X_product = diffrnce_x_inter * diffrnce_y_line - diffrnce_y_inter * diffrnce_x_line
-    
+
             if abs(X_product) < sys.float_info.epsilon * 100:
                 if st.is_between(line, line, inter_x, inter_y):
                     return True
-    
+
             return False
-    
+
 
         def calculate(st, line_i, line_j): # Getting the result of the tuple of intersecting coordinates
-    
+
             '''
             From ax+by=c ; a=y2-y1 b=x1-x2 c=x1y2-x2y1
             (a1b2-a2b1)x = b2c1 - b1c2
             (a2b1-a1b2)y = a2c1 - a1c2
-            '''    
+            '''
 
             (B_i, A_i) = tuple(map(operator.sub, line_i[1], line_i[0]))
             (B_j, A_j) = tuple(map(operator.sub, line_j[1], line_j[0]))
-    
+
             B_i = - B_i
             B_j = - B_j
-    
+
 
             C_i = line_i[0][0] * line_i[1][1] - line_i[0][1] * line_i[1][0]
             C_j = line_j[0][0] * line_j[1][1] - line_j[0][1] * line_j[1][0]
-    
+
 
             k_y = A_j * B_i - A_i * B_j
             b_y = A_j * C_i - A_i * C_j
-    
+
 
             k_x = A_i * B_j - A_j * B_i
             b_x = B_j * C_i - B_i * C_j
-    
+
 
             if k_y == 0 or k_x == 0: # there is no intersection
                 return False
             else:
                 x = b_x / k_x
                 y = b_y / k_y
-    
+
 
                 if st.is_between(line_i, line_j, x, y): # if the intersection is not on extension
                     return x, y
                 return False
-    
+
 
         def is_between(st, line_i, line_j, x, y): # Checking whether the point is between the lines
             max_x_i = max(line_i[0][0], line_i[1][0])
             min_x_i = min(line_i[0][0], line_i[1][0])
-    
+
 
             max_y_i = max(line_i[0][1], line_i[1][1])
             min_y_i = min(line_i[0][1], line_i[1][1])
-    
+
 
             max_x_j = max(line_j[0][0], line_j[1][0])
             min_x_j = min(line_j[0][0], line_j[1][0])
-    
+
 
             max_y_j = max(line_j[0][1], line_j[1][1])
             min_y_j = min(line_j[0][1], line_j[1][1])
-    
+
 
             if x >= min_x_i and x <= max_x_i and y >= min_y_i and y <= max_y_i:
                 if x >= min_x_j and x <= max_x_j and y >= min_y_j and y <= max_y_j:
@@ -333,39 +333,47 @@ class Assignment_1:
                     return False
             else:
                 return False
-    
 
-        def output_tree(st):    
+
+        def output_tree(st):
 
             # sys.stderr.write('V = {\n')
             # for v in st.V.keys():
             #    sys.stderr.write(' ' + str(v) + ": (" + "%.2f" % st.V[v][0] + "," + "%.2f" % st.V[v][1] + ")\n")
             # sys.stderr.write('}\n')
 
-            
-            sys.stderr.write("\nV " + str(len(st.V)) + '\n')
 
-            sys.stderr.write('\nE {')
+            sys.stdout.write("V " + str(len(st.V)) + '\n')
+
+            sys.stdout.write('E {')
             maxid = 0
+
             for e in st.E:
                 if maxid == len(st.E) - 1:
                     if int(e[0]) != 0 and int(e[1]) != 0:
-                        sys.stderr.write('<' + str(int(e[0])) + "," + str(int(e[1])) + '>')
+                        sys.stdout.write('<' + str(int(e[0])) + "," + str(int(e[1])) + '>')
                 else:
                     if int(e[0]) != 0 and int(e[1]) != 0:
-                        sys.stderr.write('<' + str(int(e[0])) + "," + str(int(e[1])) + '>')
-                    sys.stderr.write(',')
+                        sys.stdout.write('<' + str(int(e[0])) + "," + str(int(e[1])) + '>')
+                    sys.stdout.write(',')
                     maxid += 1
-    
-
-            sys.stderr.write('}\n')  
-
-
+            sys.stdout.write('}\n')
+            sys.stderr.flush()
+            sys.stdout.flush()
 def main():
-    
+
     vertex = Assignment_1()
     vertex.run_sim()
+    sys.stdout.flush()
+    sys.stderr.flush()
 
-    while True:
-        line = sys.stdin.readline()
-        time.sleep(0.1)
+    #while True:
+       # line = sys.stdin.readline()
+        # time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
--- ece650-a3.cpp
+++ ece650-a3.cpp
@@ -1,5 +1,138 @@
-#include <iostream>
+//#include <iostream>
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <signal.h>
+//
+//using namespace std;
+//
+//void setup_for_rgen(){
+//    int pipe_myrgen[2];
+//    close(pipe_myrgen[0]);
+//    dup2(pipe_myrgen[1], STDOUT_FILENO);
+//    close(pipe_myrgen[1]);
+//}
+//
+//void setup_for_a1(){
+//    int pipe_myrgen[2];
+//    int pipe_my_a1[2];
+//
+//    // set the input of the rgen pipe
+//    dup2(pipe_myrgen[0], STDIN_FILENO);
+//    close(pipe_myrgen[0]);
+//    close(pipe_my_a1[0]);
+//
+//    //set the output of the assignment 1 pipe
+//    dup2(pipe_my_a1[1], STDOUT_FILENO);
+//    close(pipe_my_a1[1]);
+//}
+//
+//
+//int main(int argc, char** argv) {
+//    pid_t child_myrgen;
+//    int pipe_myrgen[2];
+//
+//    // this is the pipe intended to be used between rgen and assignment 1
+//    pipe(pipe_myrgen);
+//
+//    // this is to create the child process for rgen
+//    child_myrgen = fork();
+//
+//    // if this is the child process for myrgen
+//        if (child_myrgen == 0) {
+//            setup_for_rgen();
+//
+//            // the rgen file may be in any location but the variables depend on stdin, thus argv
+//            execvp("./rgen", argv);
+//
+//        } else if (child_myrgen == -1) {
+//
+//            exit(1);
+//
+//        }
+//
+//    close(pipe_myrgen[1]);
+//
+// // the parent finishes the  random input generator fork
+//
+// // the parent starts the assignment 1 fork
+//
+//    pid_t child_a1;
+//    int pipe_my_a1[2];
+//
+//    pipe(pipe_my_a1);
+//
+//    child_a1 = fork();
+//
+//    if(child_a1 == 0) {
+//        setup_for_a1();
+//
+//    // there are multiple variables in the python file so we use execl
+//        execl("/run/bin/sh", "sh", "-c", "python ./ece650-a1.py",(char *) NULL);
+//
+//    } else if (child_a1 == -1){
+//
+//        exit(1);
+//
+//    }
+//
+//    close(pipe_myrgen[0]);
+//    close(pipe_my_a1[1]); // Because no output should be displayed from assignment 1
+//
+//	// the parent finishes the assignment 1 fork
+//
+//    FILE* writeTo_a2;
+//    writeTo_a2 = popen("./ece650-a2", "w");
+//
+//    pid_t child_a2;
+//
+//    child_a2 = fork();
+//
+//    if (child_a2 == 0) {
+//        FILE* readFrom_a1 = fdopen(pipe_my_a1[0],"r");
+//
+//        char* line = NULL;
+//
+//        while (fgets(line, 500, readFrom_a1)){
+//
+//            fputs(line, writeTo_a2);
+//            fflush(writeTo_a2);
+//
+//        }
+//
+//        fclose(readFrom_a1);
+//
+//        fclose(writeTo_a2);
+//
+//        exit(0);
+//
+//    } else if (child_a2 == -1) {
+//
+//        exit(1);
+//
+//    }
+//
+//    close(pipe_my_a1[0]); // stop reading assignment 1
+//
+//    char* s_line = NULL;
+//
+//    while(fgets(s_line, 500, stdin))
+//    {
+//        cout << s_line <<endl;
+//        fputs(s_line, writeTo_a2);
+//        fflush(writeTo_a2);
+//    }
+//
+//    pclose(writeTo_a2); // close the Assignment 2
+//
+//    kill(child_myrgen, SIGTERM);;
+//
+//    return 0;
+//}
+
 #include <stdio.h>
+#include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,126 +140,82 @@
 
 using namespace std;
 
-void setup_for_rgen(){
-    int pipe_myrgen[2];
-    close(pipe_myrgen[0]);
-    dup2(pipe_myrgen[1], STDOUT_FILENO);
-    close(pipe_myrgen[1]);
-}
-
-void setup_for_a1(){
-    int pipe_myrgen[2];
-    int pipe_my_a1[2];
-
-    // set the input of the rgen pipe
-    dup2(pipe_myrgen[0], STDIN_FILENO);
-    close(pipe_myrgen[0]);
-    close(pipe_my_a1[0]);
-
-    //set the output of the assignment 1 pipe
-    dup2(pipe_my_a1[1], STDOUT_FILENO);
-    close(pipe_my_a1[1]);
-}
-
-
-int main(int argc, char** argv) {
-    pid_t child_myrgen;
-    int pipe_myrgen[2];
-
-    // this is the pipe intended to be used between rgen and assignment 1
-    pipe(pipe_myrgen);
-
-    // this is to create the child process for rgen
-    child_myrgen = fork();
-
-    // if this is the child process for myrgen
-        if (child_myrgen == 0) {
-            setup_for_rgen();
-
-            // the rgen file may be in any location but the variables depend on stdin, thus argv
-            execvp("./rgen", argv);
-
-        } else if (child_myrgen == -1) {
-
-            exit(1);
-
-        }
-
-    close(pipe_myrgen[1]);
-
- // the parent finishes the  random input generator fork
-
- // the parent starts the assignment 1 fork
-
-    pid_t child_a1;
-    int pipe_my_a1[2];
-
-    pipe(pipe_my_a1);
-
-    child_a1 = fork();
-
-    if(child_a1 == 0) {
-        setup_for_a1();
-
-    // there are multiple variables in the python file so we use execl
-        execl("/run/bin/sh", "sh", "-c", "python ./ece650-a1.py",(char *) NULL);
-
-    } else if (child_a1 == -1){
-
-        exit(1);
-
-    }
-
-    close(pipe_myrgen[0]);
-    close(pipe_my_a1[1]); // Because no output should be displayed from assignment 1 
-
-	// the parent finishes the assignment 1 fork
-
-    FILE* writeTo_a2;
-    writeTo_a2 = popen("./ece650-a2", "w");
-
-    pid_t child_a2;
-
-    child_a2 = fork();
-
-    if (child_a2 == 0) {
-        FILE* readFrom_a1 = fdopen(pipe_my_a1[0],"r");
-
-        char* line = NULL;
-
-        while (fgets(line, 500, readFrom_a1)){
-
-            fputs(line, writeTo_a2);
-            fflush(writeTo_a2);
-
-        }
-
-        fclose(readFrom_a1);
-
-        fclose(writeTo_a2);
-
-        exit(0);
-    
-    } else if (child_a2 == -1) {
-
-        exit(1);
-    
-    }
-    
-    close(pipe_my_a1[0]); // stop reading assignment 1
-
-    char* s_line = NULL;
-
-    while(fgets(s_line, 500, stdin))
+int main(int argc, char **argv)
+{
+    int pid_rgen, pipe_r_rgen[2];
+    pipe(pipe_r_rgen);
+    switch(pid_rgen = fork())
     {
-        cout << s_line <<endl;
-        fputs(s_line, writeTo_a2);
-        fflush(writeTo_a2);
+        case -1:
+            exit(EXIT_FAILURE);
+        case 0://child process - rgen
+            close(pipe_r_rgen[0]);
+            dup2(pipe_r_rgen[1], STDOUT_FILENO);
+            close(pipe_r_rgen[1]);
+            execvp("./rgen", argv);
+            //execl("/bin/sh", "sh", "-c", , NULL);
     }
 
-    pclose(writeTo_a2); // close the Assignment 2
+    close(pipe_r_rgen[1]);
+    /*parent finish rgen fork*/
 
-    kill(child_myrgen, SIGTERM);;
+    /*parent start fork a1*/
+    int pid_a1, pipe_r_a1[2];
+    pipe(pipe_r_a1);
+    switch(pid_a1 = fork())
+    {
+        case -1:
+            exit(EXIT_FAILURE);
+        case 0: //pid_a1
+            dup2(pipe_r_rgen[0], STDIN_FILENO);
+            close(pipe_r_rgen[0]);
+            close(pipe_r_a1[0]);
+            dup2(pipe_r_a1[1], STDOUT_FILENO);
+            close(pipe_r_a1[1]);
+            execl("/bin/sh", "sh", "-c", "python ./ece650-a1.py",(char *) NULL);
+    }
+    close(pipe_r_rgen[0]);
+    close(pipe_r_a1[1]);
+    /*parent finish a1 fork*/
 
-    return 0;
+    /*parent fork a2 using popen*/
+    FILE* write_to_a2 = popen("./ece650-a2", "w");//a2-process
+
+    /*parent start fork a1 reader*/
+    int pid_a1_reader;
+    switch(pid_a1_reader = fork())
+    {
+        case -1:
+            exit(EXIT_FAILURE);
+        case 0://pid_a1_reader
+        {
+            FILE* read_from_a1 = fdopen(pipe_r_a1[0],"r");
+            char* line = NULL;
+            size_t bytes = 0;
+            while(getline(&line, &bytes, read_from_a1) != -1)
+            {
+                cout<<line;
+                fputs(line, write_to_a2);
+                fflush(write_to_a2);
+            }
+            fclose(read_from_a1);
+            fclose(write_to_a2);
+            //kill(getppid(),SIGTERM); //commented
+            exit(EXIT_SUCCESS);
+        }
+    }
+    //parent, read from stdin, fputs to a2
+    close(pipe_r_a1[0]);//does not read a1 at stdin reader
+    char* line = NULL;
+    size_t bytes = 0;
+    while(getline(&line, &bytes, stdin) != -1)
+    {
+        fputs(line, write_to_a2);
+        fflush(write_to_a2);
+    }
+    kill(pid_rgen, SIGTERM);
+    pclose(write_to_a2); //send EOF to a2
+    return EXIT_SUCCESS;
 }
+
+
